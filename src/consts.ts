27d6dfc6af589efb7bd65cc153aa706c05b85296
anghunk <anghunk@gmail.com>
2025-11-06 18:54:19 +0800

    fix: Modify some text

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '子舒的博客';
-export const SITE_DESCRIPTION = '想留下点什么，证明我来过，一个热爱生活的前端开发工程师';
+export const SITE_DESCRIPTION = '想留下点什么，证明我来过，一个热爱生活的程序员。';
--- src/content/index.md
+++ src/content/index.md
@@ -1,1 +1,5 @@
-如果你想了解更多的我，[关于我](/about/)
+
+
+<img src="https://imgurl.zishu.me/author.webp" class="author" />
+
+想留下点什么，证明我来过，一个热爱生活的程序员。[关于我](/about/)
